63c3bda48ec4202b0a1d27d94553160cb912e74d
LightningGuy123 <sak225@bath.ac.uk>
2026-03-13 13:15:05 +0000

    Added technical documentation comments for motor test code

--- PW3_Group15_MotorTestCode 2.ino
+++ PW3_Group15_MotorTestCode 2.ino
@@ -3,8 +3,10 @@ int motorPin = 8; //change the pin here
 void setup() {
   // jap200
   // initialise communication at 9600 baud rate
+  // Initialises serial communication and configures motor control pin before test execution
+  // Starts serial monitor output for debugging and observing motor behaviour
   Serial.begin(9600);
-  
+  // Sets motor pin as output so voltage can be driven to the motor interface
   pinMode(motorPin, OUTPUT);
 
 }
@@ -14,7 +16,7 @@ void loop() {
   //----------------
   // SET TEST
   //----------------
-
+// Drives motor pin high to apply voltage and test motor activation
   digitalWrite(motorPin, HIGH);
 
 
