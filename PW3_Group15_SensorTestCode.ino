084d647fb3632a692312a355d8b117fa2a430d4d
nh675 <nico.hene@outlook.com>
2026-03-19 18:21:19 +0000

    Update PW3_Group15_SensorTestCode.ino
    Added usernames, deleted unhelpful comments, removed commented out serial plotter code as not used, changed pin numbers to reflect main code

--- PW3_Group15_SensorTestCode.ino
+++ PW3_Group15_SensorTestCode.ino
@@ -1,8 +1,9 @@
-int leftSensorPin = A0; // change the pin no. here
-int rightSensorPin = A1;
+int leftSensorPin = D3; // change the pin no. here
+int rightSensorPin = D4;
+
+// hh2212 MI549 JBC64 MM3772 SAK225 TC2308
 
 void setup() {
-  // jap200
   // initialise communication at 9600 baud rate
   Serial.begin(9600);
 
@@ -29,16 +30,6 @@ void loop() {
   Serial.print(".        Difference: ");
   Serial.print(sensorDifference);
   Serial.println(".");
-
-  
-  //----------------
-  // SERIAL PLOTTER
-  // (you have to comment out the serial monitor code if you wish to use the serial plotter)
-  //----------------
-
-  // Serial.println(leftSensor);
-  // Serial.println(rightSensor);
-  // Serial.println(sensorDifference);
 
 
   delay(200);   // delay by 200ms
